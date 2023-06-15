84d3af7dc42bd73da0e515786d1cb6c287e37c1a
frog (Mugdho) <frogbid.khl@gmail.com>
2023-06-15 19:36:03 +0600

    15/06/2023

---
+++ assets/js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("img").click(function(){
+        var t = $(this).attr("src");
+        $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+        $("#myModal").modal();
+    });
+
+    $("video").click(function(){
+        var v = $("video > source");
+        var t = v.attr("src");
+        $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+        $("#myModal").modal();
+    });
+});//EOF Document.ready
+
+
+$(document).ready(function () {
+    $(".customer-logos").slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3
+                }
+            }
+        ]
+    });
+});
